cebaffb1056615a8ae6fd30e10ebe11d625d9d4c
mccarthinator <mccarthy.cath@gmail.com>
2017-08-02 20:49:17 -0500

    updates

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 var express = require('express');
 var app = express();
 var PORT = 3000;
+var roster = [
+    {
+        name: 'mark'
+    },
+    {
+        name: 'katie'
+    },
+    {
+        name: 'chooi'
+    }
+];
+
+//define GET route that returns a welcome msg
+app.get("/", function(req, res) {
+  res.send("Welcome to the student roster page!");
+});
+
+//get route that returns 3 students
+app.get("/students", function(req, res) {
+  res.json("Welcome to the student roster page!");
+});
+
 
 app.listen(PORT, function() {
 console.log(`Listening on PORT ${PORT}`);
 });
+
